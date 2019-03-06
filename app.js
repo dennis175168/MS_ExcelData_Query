97e8ab139baff2d1502d75272bf77b4c4ee88ce3
Unknown <dennis174516810344129@gmail.com>
2019-03-07 02:30:10 +0800

    Init

--- app.js
+++ app.js
@@ -2,11 +2,11 @@
 var async = require("async");
 var request = require('request');
 
-var key = "AIzaSyDKoe3HgubCTmPENDXEVQF0Lby92kH3iCo";
+var key = "";
 var fields = "name,rating,formatted_phone_number";
 
-var flow_api_get_data = 'https://prod-29.southeastasia.logic.azure.com:443/workflows/f36bf8349756429e97b854698ccce097/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=bEA5DSjealajQJk_5FrIlPA_IQSPEMlt33Ee1MxnXj8';
-var flow_api_update_data = 'https://prod-03.southeastasia.logic.azure.com:443/workflows/8a0d029c52074e0dabdea73da26bd044/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=_ghALYQcUGX8QdnTnqnLTPr8a5AOqKJ53C03kQ1vZeA';
+var flow_api_get_data = '';
+var flow_api_update_data = '';
 
 
 request(flow_api_get_data , function(error, response, body){ //get company_name in excel
